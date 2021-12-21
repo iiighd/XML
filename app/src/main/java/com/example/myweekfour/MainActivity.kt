ccaf79ee3fe01ac730f245dc3443ec59155dc4c3
Win10 <iiighd19@gmail.com>
2021-12-21 16:47:39 +0300

    -

---
+++ app/src/main/java/com/example/myweekfour/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.myweekfour
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import java.lang.Exception
+import java.util.*
+import kotlin.collections.ArrayList
+class MainActivity : AppCompatActivity() {
+    lateinit var studentsRecyclerView: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Recycler view
+        studentsRecyclerView = findViewById(R.id.students_rv)
+        studentsRecyclerView.adapter = Adapter(getStudents())
+        studentsRecyclerView.layoutManager = GridLayoutManager(this, 3)
+    }
+
+    private fun getStudents(): ArrayList<Student>{
+        var studentsList = arrayListOf<Student>()
+        try {
+            val parser = XMLParser()
+            val inputStream = assets.open("studentdetails.xml")
+            studentsList = parser.parse(inputStream)
+        } catch (e: Exception){ e.printStackTrace() }
+        return studentsList
+    }
+}
---
+++ app/src/main/java/com/example/myweekfour/Student.kt
@@ -0,0 +1,3 @@
+package com.example.myweekfour
+
+data class Student (val id: Int, val name: String, val mark: Float)
